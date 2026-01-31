6a3d2431acdeda9550315c8e3a19a094e6887938
zmercury <messiniki@outlook.com>
2026-01-31 19:49:25 +0545

    feat: dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "@/components/theme-provider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
