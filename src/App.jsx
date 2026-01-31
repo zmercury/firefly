faf2c30a91f5ae91336cce3747489b09aadb3f05
zmercury <messiniki@outlook.com>
2026-01-31 19:46:01 +0545

    chore: install shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <Button variant="destructive">Button</Button>
       </div>
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
