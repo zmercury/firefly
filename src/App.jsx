e51f145cad722cd8b47d1855c2a8e359df64620c
zmercury <messiniki@outlook.com>
2026-01-31 19:52:20 +0545

    feat: added color mode toggle from shadcn/ui
    Closes: [

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <h1 className="text-3xl font-bold underline">Hello World</h1>
         <Button variant="destructive">Button</Button>
+        <ModeToggle />
       </div>
     </>
   );
