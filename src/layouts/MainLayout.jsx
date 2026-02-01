da67f5b2e9b154346ee1bc0e516ec9a522e6462d
zmercury <messiniki@outlook.com>
2026-02-01 16:27:55 +0545

    feat: removed nav and footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,24 +1,24 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import { Outlet } from "react-router-dom";
+import { ModeToggle } from "@/components/mode-toggle";
 
-const navData = [
-  { title: "Home", href: "/" },
-  { title: "Products", href: "/products" },
-  { title: "About Us", href: "/about" },
-  { title: "Contacts", href: "/contact" },
-];
+// const navData = [
+//   { title: "Home", href: "/" },
+//   { title: "Products", href: "/products" },
+//   { title: "About Us", href: "/about" },
+//   { title: "Contacts", href: "/contact" },
+// ];
 
 function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      <Navbar navigationData={navData} />
+      {/* <Navbar navigationData={navData} /> */}
 
       <main className="flex-1 p-4">
         <Outlet />
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
+      <ModeToggle />
     </div>
   );
 }
