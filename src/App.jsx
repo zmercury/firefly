990468921c08b22a04f588d6eaeb3a1f783868df
zmercury <messiniki@outlook.com>
2026-02-01 16:02:05 +0545

    refactor: css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import HomePage from "./pages/home/HomePage";
 import MainLayout from "./layouts/MainLayout";
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
