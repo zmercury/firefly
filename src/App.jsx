50bfc80f9a963098300f60b70a89ef6e446342f2
zmercury <messiniki@outlook.com>
2026-02-01 11:34:11 +0545

    feat: made app use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/mode-toggle";
+import HomePage from "./pages/home/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello World</h1>
-        <Button variant="destructive">Button</Button>
-        <ModeToggle />
-      </div>
-    </>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "@/components/theme-provider.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { ThemeProvider } from "@/components/theme-provider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
